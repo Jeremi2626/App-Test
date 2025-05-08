b32b1143b53654bde96d20392e325e202d53c525
Jeremi Rojas <Jeremirojas2626@gmail.com>
2025-05-08 05:05:01 -0400

    Se le dio funcionalidad al buscador, se muestra la ciudad y los datos del clima actual

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import Navbar from './components/Navbar';
 import Form from './components/Form';
-import axios from 'axios';
+
+
+
 
 const App = () => {
-  const API_KEY = `${import.meta.env.VITE_API_KEY}`;
- 
+
   return (
     <div className="min-h-screen bg-gradient-to-br from-cyan-200 to-blue-300 flex items-center justify-center p-4">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
 
         <Navbar />
         <Form />
-
+      
       </div>
     </div>
   );
